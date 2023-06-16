432e970c02b02c2f41f254afd649b6464b912667
otavio <otavioagusutomperes2703@gmail.com>
2023-06-16 17:34:38 -0300

    start project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project: Landing Page for an Agency
+
+## This is a project of a landing page for an Agency made to study purpose
+
+**Based on Landingpage project using NextJs by Front Academy at Youtube**
+
+Technologies used:
+* NextJs
+* Tailwindcss
+* ESlint
+* Typescript
+
+How to run the project:
+1. Npm install 
+2. Npm run dev
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <header className="">
+        
+      </header>
+
+      <div className="">
+        <h1>Project Start</h1>
+      </div>
+
+      <footer className="">
+        
+      </footer>
+    </main>
+  )
+}
