7092204ff56b4ba57600142f03ad4e162da88a1b
otavio <otavioagusutomperes2703@gmail.com>
2023-06-18 16:46:08 -0300

    header small changes and hover effect

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -8,11 +8,11 @@ export default function Header() {
         <header className="flex flex-row items-center justify-around w-full border-b border-purple-900 py-5">
            <Image src={agencyLogo} height={60} width={174} alt="agency logo" />
            <div className="flex gap-10">
-                <Link href={'/'}>Home</Link>
-                <Link href={'/'}>O que fazemos</Link>
-                <Link href={'/'}>Cases</Link>
+                <Link className="hover:text-greenHighlight" href={'/'}>Home</Link>
+                <Link className="hover:text-greenHighlight" href={'/'}>O que fazemos</Link>
+                <Link className="hover:text-greenHighlight" href={'/'}>Cases</Link>
            </div>
-           <button className="bg-green-500 px-5 py-2 rounded">Fale conosco</button>
+           <button className="bg-greenShade px-5 py-2 rounded hover:bg-greenHighlight">Fale conosco</button>
         </header>
     )
 }
