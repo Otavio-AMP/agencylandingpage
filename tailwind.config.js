fe6b28fd084516a584a718c04f0768c4269dc57c
otavio <otavioagusutomperes2703@gmail.com>
2023-06-18 16:48:39 -0300

    background-image config and more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,25 @@ module.exports = {
     colors:{
       customPurple: '#350053',
       purple: {
+        800: 'rgba(56, 1, 88, 1)',
         900: 'rgb(88 28 135)',
         950: 'rgb(59 7 100)'
-      }
+      },
+      footerPurple: 'rgba(38, 0, 59, 1)',
+      greenHighlight: 'rgba(15, 206, 51, 1)',
+      greenShade: 'rgba(13, 199, 79, 1)',
+      greenDark: 'rgba(0, 145, 27, 1)',
+      white: 'rgba(255, 255, 255, 1)',
+      whiteShade: 'rgba(247, 247, 247, 1)',
+      gray: 'rgba(74, 74, 74, 1)',
+      black: 'rgba(0, 0, 0, 1)'
     },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'backgroundEffect': "url('../../public/backgroundEffect.svg')",
       },
     },
   },
